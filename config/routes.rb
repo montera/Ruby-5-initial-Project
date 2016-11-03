f1e45dab522a85da30aa21b0c654accbdc49fbb0
Ikhwanul Walid <iwan@mdev.pluit.dyndns-ip.com>
2016-11-03 16:56:30 +0700

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # get 'welcome/index'
 
   # get 'myurls/new'
   # resources :welcome, :only => [:show, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'	
-  
+  # root 'application#hello'
+  root 'users#index'
+    
 end
